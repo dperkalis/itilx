5dbabc4b2863d537bc8b775c59a4664b2c30ed8e
dperkalis <dperkalis@hotmail.com>
2014-09-15 23:00:20 -0400

    Refactored Storage Service.

--- js/controllers/addQuizCtrl.js
+++ js/controllers/addQuizCtrl.js
@@ -1,12 +1,38 @@
 (function () {
     var module = angular.module("itilxControllers");
-    module.controller("addQuizCtrl", ["$scope", "storageSvc", "stateSvc",
-        function ($scope, storageSvc, stateSvc) {
+    module.controller("addQuizCtrl", ["$scope", "$location", "storageSvc", "stateSvc",
+        function ($scope, $location, storageSvc, stateSvc) {
+
+            $scope.quiz = { id: "", quizName: "", quizDescription: ""};
+            $scope.questions = [];
+            $scope.question = { id: "", quizId: "", questionText: "", answer1: "", answer2: "", answer3: "", answer4: ""};
+
+
+            if (!stateSvc.quizid) {
+                $scope.mode = "add";
+                $scope.question = { id: "", quizId: "", questionText: "", answer1: "", answer2: "", answer3: "", answer4: ""};
+                $scope.showQuestions = false;
+                $scope.questionCount = 0;
+                $scope.showDelete = false;
+            } else {
+                $scope.mode = "edit";
+                $scope.showQuestions = true;
+                $scope.questionCount = 0;
+                $scope.showDelete = true;
+
+                bindQuiz(stateSvc.quizid);
+            }
 
             function bindQuestions(quizid) {
                 storageSvc.query("questions", "", {quizid: quizid})
                     .then(function (data) {
-                        $scope.questions = data;
+                        var temp =[];
+                        angular.forEach(data, function(item) {
+                            temp = temp.concat({
+                                id: item.id,
+                                question: angular.fromJson(item.question)});
+                        });
+                        $scope.questions = temp;
                         $scope.$apply();
                     });
             };
@@ -28,42 +54,32 @@
             }
 
 
-            $scope.quiz = { id: "", quizName: "", quizDescription: ""};
-            $scope.questions = [];
-            $scope.question = { id: "", quizId: "", questionText: "", answer1: "", answer2: "", answer3: "", answer4: ""};
 
-
-            if (!stateSvc.quizid) {
-                $scope.mode = "add";
-                $scope.question = { id: "", quizId: "", questionText: "", answer1: "", answer2: "", answer3: "", answer4: ""};
-                $scope.showQuestions = false;
-                $scope.questionCount = 0;
-            } else {
-                $scope.mode = "edit";
-                $scope.showQuestions = true;
-                $scope.questionCount = 0;
-
-                bindQuiz(stateSvc.quizid);
-            }
 
             $scope.deleteQuiz = function() {
-
                 var quizid = stateSvc.quizid;
+                alert("deleting quiz")
 
                 storageSvc.del("questions", {quizid: quizid})
                   .then(function(data) {
-                      alert("questions deleted");
-                      bindQuestions(quizid);
+                        alert('asdfasdf')
+
                   });
 
                 storageSvc.del("quizzes", {id:quizid})
                     .then(function(data) {
-                        alert("quiz deleted!");
                         bindQuiz(quizid);
+
+                        $scope.mode = "add";
+                        $scope.showQuestions = false;
+                        $scope.showDelete = false;
+                        $location.path("/admin");
+                        $scope.apply();
                     });
             };
 
             $scope.deleteQuestion = function(questionid) {
+                alert(questionid)
               storageSvc.del("questions", {id:questionid})
                   .then(function(data) {
                       bindQuestions(stateSvc.quizid);
@@ -80,6 +96,8 @@
                         $scope.showQuestions = true;
                         $scope.mode = "edit";
                         stateSvc.quizid = data.id;
+                        $location.path("/admin/" + data.id);
+                        stateSvc.quizid = data.id;
                         $scope.$apply();
                     });
                 } else {
@@ -94,13 +112,18 @@
             };
 
             $scope.addQuestion = function () {
+                var question = {
+                    questionText: $scope.question.questionText,
+                    answers : [
+                        {answerid:1, answer1: $scope.question.answer1},
+                        {answerid:2, answer1: $scope.question.answer2},
+                        {answerid:3, answer1: $scope.question.answer3},
+                        {answerid:4, answer1: $scope.question.answer4}]
+                };
+
                 storageSvc.insert("questions", {
                     quizId: $scope.quiz.id,
-                    questionText: $scope.question.questionText,
-                    answer1: $scope.question.answer1,
-                    answer2: $scope.question.answer2,
-                    answer3: $scope.question.answer3,
-                    answer4: $scope.question.answer4
+                    question: angular.toJson(question)
                 }).then(function (data) {
                     $scope.questions = $scope.questions.concat(data);
                     $scope.questionCount = $scope.questions.length;
